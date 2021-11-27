603883cd44353a5b02d9084bef2f740c20f62098
codingmohamed <cloudchronicles92@gmail.com>
2021-11-27 02:12:23 +0000

    image/dom refreshes with search

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ let clickInput = document.querySelector(`.searchButton`)
 // search bar defined
 let enterInput = document.querySelector(`.searchPokemon`)
 
+//if enter pressed, a click occurs, function getSearch is called
 enterInput.addEventListener(`keyup`, getEnter)
+// if button clicked, input stored in userInput
 clickInput.addEventListener(`click`, getSearch)
+
+let spriteLocation = document.querySelector(`.sprite-location`)
 
 function getEnter(enter) {
     enter.preventDefault();
@@ -13,7 +17,24 @@ function getEnter(enter) {
     }
 }
 
+async function getPokemon(pokemon) {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`);
+    console.log(response)
+    let data = await response.json();
+    console.log(data);
+
+    let quotesSprite = JSON.stringify(data.sprites.front_default);
+    let pokemonSprite = quotesSprite.replaceAll('"', '');
+    let img = document.createElement("img");
+    img.src = pokemonSprite;
+    spriteLocation.innerHTML = "";
+    spriteLocation.append(img);
+    img.style.height = `200px`;
+    img.style.width = `200px`;
+}
+
 function getSearch(userInput) {
     userInput = enterInput.value;
-    console.log(userInput);
+    getPokemon(userInput);
+
 }
